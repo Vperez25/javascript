3c36f3b97a0e2ed1f63d18413705fa6407e91182
Vperez25 <vperez_22@alu.uabcs.mx>
2024-11-08 22:42:45 -0700

    se agrego ejercicio 2 JS

---
+++ Ejercicio2/index.js
@@ -0,0 +1,22 @@
+let cuerpo = document.getElementById("cuerpo")
+
+function changeColorRed(){
+
+    cuerpo.style.backgroundColor = "red";
+
+}
+function changeColorGreen(){
+
+    cuerpo.style.backgroundColor = "green";
+
+}
+function changeColorBlue(){
+
+    cuerpo.style.backgroundColor = "blue";
+}
+
+
+function reset(){
+
+    cuerpo.style.backgroundColor = "white";
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # javascript
 
 - [Ejercicio 1 - Calculadora](./Ejercicio1)
+- [Ejercicio 2 - Manipulación de estilos](./Ejercicio2)
